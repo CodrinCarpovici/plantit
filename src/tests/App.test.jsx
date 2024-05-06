52fd4b79fec4194819774d0d44260a1b435015b1
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-05-06 16:33:39 +0100

    tests and structure set up done

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import App from "../App";
+
+// Tests if App renders without issues
+it("App Component Renders Without Error", () => {
+  render(<App />);
+});
