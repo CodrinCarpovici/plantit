446d96a0ca9b35da95d44955682d6d0c8ad9de73
Dimi Tsonev <dconev@gmail.com>
2024-05-08 17:12:09 +0100

    html navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.scss'
 import './index.scss'
+import Navbar from './components/Navbar'
+// import MyPlans from './pages/Plantpage'
+// import Calendar from './pages/Calendarpage'
+// import News from './pages/Newspage'
+// import Forum from './pages/Forumpage'
+// import Home from './pages/Homepage'
+// import Contact from './pages/Contactpage'
+// // import SignIn from './pages/'
+// // import Recognition from './pages/'
 
 function App() {
 
   return (
     <>
-    <h1>Hello</h1>
+    <Navbar/>
     </>
   )
 }
 
 export default App
+
+
+// {/* <Router>
+//       <Navbar />
+//       <Routes>
+//       <Route path="/" element={<Home/>} />
+//       <Route path="/myplans" element={<MyPlans/>} />
+//       <Route path="/calendar" element={<Calendar/>} />
+//       {/* <Route path="/recognition" element={<Recognition/>} /> */}
+//       <Route path="/news" element={<News/>} />
+//       <Route path="/forum" element={<Forum/>} />
+//       <Route path="/contact" element={<Contact/>} />
+//       {/* <Route path="/signIn" element={<SignIn/>} /> */}
+//       </Routes>
+//     </Router> */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/main-logo-text icon.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className='container'>
+        <div className="row align-items-center">
+          <div className="col-lg-2">
+            <a className="navbar-brand" href="/">
+              <img className="main-logo" src={logo} alt="main-logo" />
+            </a>
+          </div>
+          <div className="col-lg-8">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/myplans">
+                    My Plans
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/calendar">
+                    Calendar
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/recognition">
+                    Recognition
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/news">
+                    News
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/forum">
+                    Forum
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-2  text-end">
+            <a className="nav-link" href="/signing">
+              Sign in
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
