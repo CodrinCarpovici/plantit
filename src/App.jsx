48fc525f986959b89e81d5cd841eeb63140420f5
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-05-08 16:37:31 +0100

    structured the footer elements (HTML)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.scss'
 import './index.scss'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
     <h1>Hello</h1>
+    <Footer></Footer>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import mainLogo from "../assets/main-logo-icon.png";
+import linkedInIcon from "../assets/linkedin-icon.png";
+import githubIcon from "../assets/github-icon.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div id="footer" className="container">
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col">
+            <img src={mainLogo} alt="main logo" />
+          </div>
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <img src={linkedInIcon} alt="" />
+              <img src={githubIcon} alt="" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex flex-column align-items-end">
+              <a href="#">Home</a>
+              <a href="">My Plants</a>
+              <a href="">Contact</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
