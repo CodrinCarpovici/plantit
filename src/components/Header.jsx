8f766dd8f83be3ac33c3657428cc45ddcc6ed498
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-05-15 18:36:30 +0100

    finilized style for header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
     <header className="header-container d-flex flex-column justify-content-center align-items-center text-center">
       <div id="container-fluid">
         <img src={cactus} alt="cactus" className="header-img" />
-        <div className="header-coontent">
-          <h1 className="header-title"> Plant iT </h1>
-          <h2 className="header-subtitle">Make your plant dreams a reality</h2>
+        <div className="header-content">
+          <h1 className="header-title" data-text="Plant iT"> Plant iT </h1>
+          <h2 className="header-subtitle" data-text="Make your plant dreams a reality">Make your plant dreams a reality</h2>
           <Searchbar />
           <Filter />
         </div>
