bb26bff816f9a79f0a3a5c61796404dc7ff35453
Dimi Tsonev <dconev@gmail.com>
2024-05-15 16:33:25 +0100

    links centered , sing in on the right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import logo from "../assets/main-logo-text-icon.png";
 import { motion } from "framer-motion";
 
-
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container">
         <div className="navbar-brand">
-        <a href="/">
+          <a href="/">
             <motion.img
               whileHover={{
                 scale: 1.1,
@@ -20,6 +19,7 @@ const Navbar = () => {
             />
           </a>
         </div>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -31,48 +31,91 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/"
+              >
                 Home
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/myplans">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/myplans"
+              >
                 My Plans
-              </a>
-              </motion.li>
-            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/calendar">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/calendar"
+              >
                 Calendar
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/recognition">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/recognition"
+              >
                 Recognition
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/news">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/news"
+              >
                 News
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/forum">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/forum"
+              >
                 Forum
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/contact">
+              </motion.a>
+            </li>
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/contact"
+              >
                 Contact
-              </a>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="nav-item">
-              <a className="nav-link" href="/signin">
+              </motion.a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <motion.a
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                className="nav-link"
+                href="/signin"
+              >
                 Sign In
-              </a>
-              </motion.li>
+              </motion.a>
+            </li>
           </ul>
         </div>
       </div>
