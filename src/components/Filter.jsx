d05c85220f7e39a719006c11ff241a8998cca5de
Dimi Tsonev <dconev@gmail.com>
2024-05-17 17:12:00 +0100

    filterbar completed

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { IoFilter } from "react-icons/io5";
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <div className="container mt-3 mb-3">
+      <div className="row justify-content-center">
+        <div className="">
+          <div className="filter-container">
+            <motion.button
+              className="btn filter-icon"
+              type="button"
+              whileHover={{
+                scale: 1.05,
+                textShadow: "0px 0px 4px gray",
+              }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <IoFilter />
+            </motion.button>
+            <div className="filter-buttons">
+              <motion.button
+                className="btn filter-button"
+                type="button"
+                whileHover={{
+                  scale: 1.05,
+                  textShadow: "0px 0px 4px gray",
+                }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Outdoor
+              </motion.button>
+              <motion.button
+                className="btn filter-button"
+                type="button"
+                whileHover={{
+                  scale: 1.05,
+                  textShadow: "0px 0px 4px gray",
+                }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Indoor
+              </motion.button>
+              <motion.button
+                className="btn filter-button"
+                type="button"
+                whileHover={{
+                  scale: 1.05,
+                  textShadow: "0px 0px 4px gray",
+                }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Filter 3
+              </motion.button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Filter
+export default Filter;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,7 +5,7 @@ const Searchbar = () => {
   return (
     <div className="container mt-3 mb-3">
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="">
           <div className="input-group">
             <input
               type="text"
