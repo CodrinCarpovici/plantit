296eddd9c5a174a474ab2646f92cf0094f2d9212
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2024-05-22 18:10:19 +0100

    added extra styling and structure to the garden page

--- src/pages/Gardenpage.jsx
+++ src/pages/Gardenpage.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import Filter from '../components/Filter'
-import DetailCard from '../components/DetailCard'
+import React from "react";
+import Filter from "../components/Filter";
+import DetailCard from "../components/DetailCard";
 
 const Gardenpage = () => {
   return (
-    <div id='garden-page' className='container-fluid'>
+    <div id="garden-page" className="container-fluid">
       <div className="d-flex flex-column justify-content-center align-items-center text-center">
-        <h1 id='garden-header'>My Plants</h1>
+        <h1 id="garden-header" className="header-title">My Garden</h1>
         <Filter />
         {/* Map the cards saved in my plants page */}
-        <DetailCard />
+        <div className="row">
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gardenpage
+export default Gardenpage;
